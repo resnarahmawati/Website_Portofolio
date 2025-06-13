ec68a6c428a5b4fe026e61accb30a42b31ce8395
= <=>
2025-06-14 06:00:53 +0700

    My responsive portfolio

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ const menuToggle = document.querySelector('.menu-toggle');
   menuToggle.addEventListener('click', () => {
     nav.classList.toggle('show');
   });
+
+  const elements = document.querySelectorAll(
+  '.fade-in-scroll, .slide-in-top, .slide-in-left, .slide-in-right'
+);
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+      //observer.unobserve(entry.target);
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+elements.forEach(el => observer.observe(el));
